fdaa375bfd061c422489ecf955d83d490d6f5c89
Luchezar Mihaylov <Luchezar.Mihaylov@intracol.com>
2016-11-29 12:10:45 +0200

    Change console demo file.

--- demo/console/demo_cli.php
+++ demo/console/demo_cli.php
@@ -1,11 +1,11 @@
 #!/usr/bin/env php
 <?php
 
-//import composer autoload file
-require_once dirname(__DIR__) . implode(DIRECTORY_SEPARATOR, ['.', '..', 'vendor', 'autoload.php']);
-
 define('EXIT_NORMAL_CODE', 0);
 define('EXIT_ERROR_CODE', 1);
+
+//import composer autoload file
+require_once dirname(__DIR__) . implode(DIRECTORY_SEPARATOR, ['.', '..', 'vendor', 'autoload.php']);
 
 //create new instance and generate random password with default settings
 //the default settings are: upperCase, lowerCase and digits
